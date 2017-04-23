4d104e09b9458f7267b792562d02a8be21953132
Curtis Ekstrom <code@fauxton.io>
2017-04-23 14:49:47 +0200

    Change import path

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/octicons/build/font/octicons.css');
-    app.import(app.bowerDirectory + '/octicons/build/font/octicons.eot', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/octicons/build/font/octicons.woff2', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/octicons/build/font/octicons.woff', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/octicons/build/font/octicons.ttf', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/octicons/build/font/octicons.svg', { destDir: 'assets' });
+    app.import(app.bowerDirectory + '/octicons/lib/octicons.css');
   }
 };
